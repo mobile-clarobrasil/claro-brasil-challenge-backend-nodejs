37d7d46322475fb2844eed7f9d7da8620035d504
Ender <luizimbroisi@gmail.com>
2019-09-14 18:48:25 -0300

    add: first commit

---
+++ handler.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports.hello = async event => {
+  const { id, deviceId } = event.pathParameters;
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: `Go Serverless v1.0! Your function executed successfully!  ${id} ${deveiceId} `,
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};

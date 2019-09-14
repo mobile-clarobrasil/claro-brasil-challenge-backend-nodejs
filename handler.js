aef01b143c973073102e3ea578d8727759cdc1bf
Ender <luizimbroisi@gmail.com>
2019-09-14 19:16:29 -0300

    add: travis.yml and secure keys

--- handler.js
+++ handler.js
@@ -5,7 +5,7 @@ exports.handler = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: `Go Serverless v1.0! Your function executed successfully!  ${id} ${deveiceId} `,
+        message: `Your function executed successfully!  ${id} ${deveiceId} `,
         input: event,
       },
       null,
